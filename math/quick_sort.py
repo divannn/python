507f67346f3800de65fc30fd77bbfafe546d2f51
divannn <unique-id@mail.ru>
2013-02-11 17:14:53 +0400

    Added quick sort sketch.

--- math/quick_sort.py
+++ math/quick_sort.py
@@ -10,37 +10,37 @@ def quick_sort(list, left, right):
 	N = len(list)
 	if N <= 1:
 		return list
-	partition(list)
-	left = quick_sort(list[0:middle])
-	right = quick_sort(list[middle: N])
-	return merge(left, right)
+	pivot = list[0]#just take the 1st item.
+	partition(list, left, right, pivot)
+	quick_sort(list, left, right)
+	quick_sort(list, left, right)
 
 # merges two sorted arrays.
-def partition(list):
-	if (len(list) == 1):
+def partition(list, left, right, pivot):
+	if (left >= right):
 		return
-	pivot = list[0]
-	i = 0
-	j = len(list) - 1
+	i = left
+	j = right
 	while i <= j:
-		while list[i] >= pivot:
+		while list[i] <= pivot:
 			i += 1
-		while list[j] < pivot:
+		while list[j] >= pivot:
 			j -= 1
 		#swap
-		tmp = list[i]
-		list[i] = list[j]
-		list[j] = tmp
+		if (i < j):
+			tmp = list[i]
+			list[i] = list[j]
+			list[j] = tmp
+
 
 def main():
 	print "-=QUICK SORT=-"
 
 	list = [5, 2, 6, 3, 1, 4, 7, 9, 0, 8]
 
-	#print ' input:' + str(list)
-	#print ' input:' + str(list)
-	#result = quick_sort(list, 0, len(list) - 1)
-	#print 'result:' + str(result)
+	print ' input:' + str(list)
+	result = quick_sort(list, 0, len(list) - 1)
+	print 'result:' + str(result)
 
 if __name__ == '__main__':
 	main()
