e08d123151b50ba3f328a97d8e3c74f55028eb35
jolt77 <divannn@mail.ru>
2013-05-12 00:40:12 +0400

    Added class method

--- basic/static_class_members.py
+++ basic/static_class_members.py
@@ -3,6 +3,7 @@
 
 __author__ = 'ivan'
 
+
 class Clazz:
     static_member = "I'm static member"
 
@@ -20,12 +21,23 @@ class Clazz:
 
     static_method2 = staticmethod(static_method2)
 
+    @classmethod
+    def class_method1(cls):
+        print "Class method 1 called"
+        print Clazz.static_member
+
+
 def main():
     clazz = Clazz()
     print clazz.static_member + "     and...     " + Clazz.static_member
+    print "Static methods" + "-" * 50
+    Clazz.static_method1()#via class
+    clazz.static_method2()#via instance
 
-    Clazz.static_method1()
-    Clazz.static_method2()
+    print "Class methods" + "-" * 50
+    Clazz.class_method1()#via class
+    clazz.class_method1()#via instance
+
 
 if __name__ == '__main__':
-	main()
+    main()
